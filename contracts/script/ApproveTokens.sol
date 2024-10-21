f7c6dec668ed2e7c368dbbdaaca9f2076bd832a9
SovaSniper <SovaSniper@outlook.com>
2024-10-21 20:55:42 +1100

    add next step

---
+++ contracts/script/ApproveTokens.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {Constants} from "./base/Constants.sol";
+import {Config} from "./base/Config.sol";
+
+contract ApproveTokens is Constants, Config {
+    function tokenApprovals() public {
+        if (!currency0.isAddressZero()) {
+            token0.approve(address(PERMIT2), type(uint256).max);
+            PERMIT2.approve(address(token0), address(posm), type(uint160).max, type(uint48).max);
+        }
+        if (!currency1.isAddressZero()) {
+            token1.approve(address(PERMIT2), type(uint256).max);
+            PERMIT2.approve(address(token1), address(posm), type(uint160).max, type(uint48).max);
+        }
+    }
+}
